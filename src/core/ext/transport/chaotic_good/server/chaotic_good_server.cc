grpc/grpc

[export] Chaotic Good: Verify Peer in Chaotic Good Handshake during Data Endpoint creation
Since Chaotic Good enables using a group of TCP connections as a composite channel we need to ensure that all TCP connections are established with the same peer. In this change, we store a Ref to the `grpc_auth_context` of the Connection that created the Control Endpoint and compare it to the `grpc_auth_context` of the Connection requesting each Data Endpoint using the [Injectable Peer Comparison API](https://github.com/grpc/grpc/pull/39610). If no peer comparison API is installed, the identity verification will not be performed.

The updated Chaotic Good handshake is as follows: (changed steps are in **bolded**)

First the control channel is established:
   1. ALTS/TLS/LOAS/PSP: Each new TCP connection goes through the “normal” security handshakes for gRPC, checking certificates, establishing identity
   2. A Chaotic Good Settings frame is sent from the client, with data_channel == 0
   3. The server processes the received Settings frame, creates N pending data connections, and responds with a Settings frame with a randomly generated set of connection ids: 1 per requested data connection. **The created PendingDataConnections hold a reference to the Control Channel’s grpc_auth_context.**
   4. The client processes the received Settings frame and creates one data connection per received connection_id.

For each data channel requested:
   1. The TCP connection proceeds as usual (same as 1 above)
   2. The Settings frame sent will relay the connection_id for this data channel, with data_channel == 1
   3. The server responds with a Settings frame with data_channel == 1.
   4. **Finally, server looks up the association for this connection_id and verifies the equivalence of the current connection’s grpc_auth_context and the stored grpc_auth_context of the control channel.**
      - **If lookup is successful and peer is equivalent, we bind the connection with that chaotic good chann

src/core/ext/transport/chaotic_good/server/chaotic_good_server.cc
@@ -68,6 +68,7 @@
 #include "src/core/lib/transport/promise_endpoint.h"
 #include "src/core/server/server.h"
 #include "src/core/telemetry/metrics.h"
+#include "src/core/transport/auth_context.h"
 #include "src/core/util/orphanable.h"
 #include "src/core/util/ref_counted_ptr.h"
 #include "src/core/util/status_helper.h"
@@ -329,7 +330,8 @@ ChaoticGoodServerListener::DataConnectionListener::DataConnectionListener(
       connect_timeout_(connect_timeout) {}
 
 PendingConnection
-ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection() {
+ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection(
+    const ChannelArgs& handshake_result_args) {
   MutexLock lock(&mu_);
   std::string connection_id;
   while (true) {
@@ -347,38 +349,59 @@ ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection() {
       [connection_id, self = WeakRefAsSubclass<DataConnectionListener>()]() {
         self->ConnectionTimeout(connection_id);
       });
-  pending_connections_.emplace(connection_id,
-                               PendingConnectionInfo{latch, timeout_task});
+  pending_connections_.emplace(
+      connection_id,
+      PendingConnectionInfo{
+          latch, timeout_task,
+          handshake_result_args.GetObjectRef<grpc_auth_context>()});
   return PendingConnection(connection_id,
                            Map(latch->Wait(), [latch](auto x) { return x; }));
 }
 
-ChaoticGoodServerListener::DataConnectionListener::PromiseEndpointLatchPtr
-ChaoticGoodServerListener::DataConnectionListener::Extract(
+auto ChaoticGoodServerListener::DataConnectionListener::Extract(
     absl::string_view id) {
   MutexLock lock(&mu_);
   auto ex = pending_connections_.extract(id);
   if (!ex.empty()) {
     event_engine_->Cancel(ex.mapped().timeout);
-    return std::move(ex.mapped().latch);
   }
-  return nullptr;
+  return ex;
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::ConnectionTimeout(
     absl::string_view id) {
-  auto latch = Extract(id);
-  if (latch != nullptr) {
-    latch->Set(absl::DeadlineExceededError("Connection timeout"));
+  auto pending_connection = Extract(id);
+  if (!pending_connection.empty()) {
+    pending_connection.mapped().latch->Set(
+        absl::DeadlineExceededError("Connection timeout"));
   }
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::FinishDataConnection(
-    absl::string_view id, PromiseEndpoint endpoint) {
-  auto latch = Extract(id);
-  if (latch != nullptr) {
-    latch->Set(std::move(endpoint));
+    absl::string_view id, PromiseEndpoint endpoint,
+    RefCountedPtr<grpc_auth_context> auth_context) {
+  auto pending_connection = Extract(id);
+  if (pending_connection.empty()) {
+    GRPC_TRACE_LOG(chaotic_good, ERROR) << "FinishDataConnection: connection "
+                                           "not found";
+    return;
   }
+
+  if (pending_connection.mapped().control_endpoint_auth_context != nullptr) {
+    // Compare the auth context with the one from the control endpoint.
+    // If they are different, drop the connection.
+    std::optional<bool> is_same_peer = auth_context->CompareAuthContext(
+      pending_connection.mapped().control_endpoint_auth_context.get());
+    if (is_same_peer.has_value() && !is_same_peer.value()) {
+      GRPC_TRACE_LOG(chaotic_good, ERROR)
+          << "FinishDataConnection: auth context mismatch";
+      pending_connection.mapped().latch->Set(
+          absl::UnauthenticatedError("Auth context mismatch"));
+      return;
+    }
+  }
+
+  pending_connection.mapped().latch->Set(std::move(endpoint));
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::Orphaned() {
@@ -490,8 +513,9 @@ auto ChaoticGoodServerListener::ActiveConnection::HandshakingState::
                                ->data_connection_listener_;
                       for (int i = 0; i < num_data_connections; i++) {
                         config.ServerAddPendingDataEndpoint(
-                            data_connection_listener.RequestDataConnection());
-                      }
+                            data_connection_listener.RequestDataConnection(
+                                self->connection_->handshake_result_args()));
+                        }
                       self->data_.emplace<ControlConnection>(std::move(config));
                     }
                     return !frame.body.data_channel();
@@ -554,7 +578,9 @@ auto ChaoticGoodServerListener::ActiveConnection::HandshakingState::
                   self->connection_->listener_->data_connection_listener_
                       ->FinishDataConnection(
                           std::get<DataConnection>(self->data_).connection_id,
-                          std::move(self->connection_->endpoint_));
+                          std::move(self->connection_->endpoint_),
+                          self->connection_->handshake_result_args_
+                              ->GetObjectRef<grpc_auth_context>());
                   return absl::OkStatus();
                 });
 }

CodeLinaro-mirror/yocto-mirrors_github_grpc_grpc

src/core/ext/transport/chaotic_good/server/chaotic_good_server.cc
@@ -63,6 +63,7 @@
 #include "src/core/lib/transport/promise_endpoint.h"
 #include "src/core/server/server.h"
 #include "src/core/telemetry/metrics.h"
+#include "src/core/transport/auth_context.h"
 #include "src/core/util/grpc_check.h"
 #include "src/core/util/orphanable.h"
 #include "src/core/util/ref_counted_ptr.h"
@@ -329,7 +330,8 @@ ChaoticGoodServerListener::DataConnectionListener::DataConnectionListener(
       connect_timeout_(connect_timeout) {}
 
 PendingConnection
-ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection() {
+ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection(
+    const ChannelArgs& handshake_result_args) {
   MutexLock lock(&mu_);
   std::string connection_id;
   while (true) {
@@ -347,38 +349,70 @@ ChaoticGoodServerListener::DataConnectionListener::RequestDataConnection() {
       [connection_id, self = WeakRefAsSubclass<DataConnectionListener>()]() {
         self->ConnectionTimeout(connection_id);
       });
-  pending_connections_.emplace(connection_id,
-                               PendingConnectionInfo{latch, timeout_task});
+  pending_connections_.emplace(
+      connection_id,
+      PendingConnectionInfo{
+          latch, timeout_task,
+          handshake_result_args.GetObjectRef<grpc_auth_context>()});
   return PendingConnection(connection_id,
                            Map(latch->Wait(), [latch](auto x) { return x; }));
 }
 
-ChaoticGoodServerListener::DataConnectionListener::PromiseEndpointLatchPtr
+std::optional<
+    ChaoticGoodServerListener::DataConnectionListener::PendingConnectionInfo>
 ChaoticGoodServerListener::DataConnectionListener::Extract(
     absl::string_view id) {
   MutexLock lock(&mu_);
   auto ex = pending_connections_.extract(id);
-  if (!ex.empty()) {
-    event_engine_->Cancel(ex.mapped().timeout);
-    return std::move(ex.mapped().latch);
+  if (ex.empty()) {
+    return std::nullopt;
   }
-  return nullptr;
+
+  event_engine_->Cancel(ex.mapped().timeout);
+  return ex.mapped();
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::ConnectionTimeout(
     absl::string_view id) {
-  auto latch = Extract(id);
-  if (latch != nullptr) {
-    latch->Set(absl::DeadlineExceededError("Connection timeout"));
+  std::optional<PendingConnectionInfo> pending_connection = Extract(id);
+  if (pending_connection == std::nullopt) {
+    GRPC_TRACE_LOG(chaotic_good, ERROR)
+        << "ConnectionTimeout: connection not found";
+    return;
   }
+  pending_connection->latch->Set(
+      absl::DeadlineExceededError("Connection timeout"));
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::FinishDataConnection(
-    absl::string_view id, PromiseEndpoint endpoint) {
-  auto latch = Extract(id);
-  if (latch != nullptr) {
-    latch->Set(std::move(endpoint));
+    absl::string_view id, PromiseEndpoint endpoint,
+    RefCountedPtr<grpc_auth_context> auth_context) {
+  auto pending_connection = Extract(id);
+  if (pending_connection == std::nullopt) {
+    GRPC_TRACE_LOG(chaotic_good, ERROR) << "FinishDataConnection: connection "
+                                           "not found";
+    return;
   }
+
+  if (pending_connection->control_endpoint_auth_context == nullptr) {
+    GRPC_TRACE_LOG(chaotic_good, INFO)
+        << "Control endpoint auth context is null, skipping auth context "
+           "comparison";
+  } else {
+    // Compare the auth context with the one from the control endpoint.
+    // If they are different, drop the connection.
+    std::optional<bool> is_same_peer = auth_context->CompareAuthContext(
+        pending_connection->control_endpoint_auth_context.get());
+    if (is_same_peer.has_value() && !is_same_peer.value()) {
+      GRPC_TRACE_LOG(chaotic_good, ERROR)
+          << "FinishDataConnection: auth context mismatch";
+      pending_connection->latch->Set(
+          absl::UnauthenticatedError("Auth context mismatch"));
+      return;
+    }
+  }
+
+  pending_connection->latch->Set(std::move(endpoint));
 }
 
 void ChaoticGoodServerListener::DataConnectionListener::Orphaned() {
@@ -490,7 +524,8 @@ auto ChaoticGoodServerListener::ActiveConnection::HandshakingState::
                                ->data_connection_listener_;
                       for (int i = 0; i < num_data_connections; i++) {
                         config.ServerAddPendingDataEndpoint(
-                            data_connection_listener.RequestDataConnection());
+                            data_connection_listener.RequestDataConnection(
+                                self->connection_->handshake_result_args()));
                       }
                       self->data_.emplace<ControlConnection>(std::move(config));
                     }
@@ -554,7 +589,9 @@ auto ChaoticGoodServerListener::ActiveConnection::HandshakingState::
                   self->connection_->listener_->data_connection_listener_
                       ->FinishDataConnection(
                           std::get<DataConnection>(self->data_).connection_id,
-                          std::move(self->connection_->endpoint_));
+                          std::move(self->connection_->endpoint_),
+                          self->connection_->handshake_result_args_
+                              ->GetObjectRef<grpc_auth_context>());
                   return absl::OkStatus();
                 });
 }